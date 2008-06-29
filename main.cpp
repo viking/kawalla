f2c9125e1622c9302ba6f508bab45f1affa516b4
Jeremy Stephens <viking415@gmail.com>
2008-06-29 10:35:32 -0500

    Renamed kflickr to kawalla

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main( int argc, char **argv )
 {
-  KApplication a( argc, argv, "kflickr" );
+  KApplication a( argc, argv, "kawalla" );
 
   MainWindow *window = new MainWindow( "Flickr Wallpaper Grabber" );
   window->resize( 400, 600 );
